266f9dfa7091f2033d0e9e600d458ccfd5bc4456
welldone82941629 <welldone82941629.3@gmail.com>
2022-12-23 16:18:24 +0800

    added new files

---
+++ script.js
@@ -0,0 +1,159 @@
+function showFrontPage(){
+    const page = document.getElementsByClassName("page");
+    const frontPage = document.getElementById("frontPage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none";
+    }
+
+    frontPage.style.display= "block";
+
+    const tab = document.getElementsByClassName("tab");
+    const showFrontPage = document.getElementById("showFrontPage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showFrontPage.style.color= "#990000";
+}
+
+function showNewsPage(){
+    const page = document.getElementsByClassName("page");
+    const newsPage = document.getElementById("newsPage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none";
+    }
+
+    newsPage.style.display= "block";
+
+    const tab = document.getElementsByClassName("tab");
+    const showNewsPage = document.getElementById("showNewsPage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showNewsPage.style.color= "#990000";
+}
+
+function showServicePage(){
+    const page = document.getElementsByClassName("page");
+    const servicePage = document.getElementById("servicePage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none"
+    }
+
+    servicePage.style.display= "block"
+
+    const tab = document.getElementsByClassName("tab");
+    const showServicePage = document.getElementById("showServicePage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showServicePage.style.color= "#990000";
+}
+
+function showHealthPage(){
+    const page = document.getElementsByClassName("page");
+    const healthPage = document.getElementById("healthPage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none"
+    }
+
+    healthPage.style.display= "block"
+
+    const tab = document.getElementsByClassName("tab");
+    const showHealthPage = document.getElementById("showHealthPage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showHealthPage.style.color= "#990000";
+}
+
+function showJoinPage(){
+    const page = document.getElementsByClassName("page");
+    const joinPage = document.getElementById("joinPage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none"
+    }
+
+    joinPage.style.display= "block"
+
+    const tab = document.getElementsByClassName("tab");
+    const showJoinPage = document.getElementById("showJoinPage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showJoinPage.style.color= "#990000";
+}
+
+function showCasePage(){
+    const page = document.getElementsByClassName("page");
+    const casePage = document.getElementById("casePage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none"
+    }
+
+    casePage.style.display= "block"
+
+    const tab = document.getElementsByClassName("tab");
+    const showCasePage = document.getElementById("showCasePage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showCasePage.style.color= "#990000";
+}
+
+function showLocationPage(){
+    const page = document.getElementsByClassName("page");
+    const locationPage = document.getElementById("locationPage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none"
+    }
+
+    locationPage.style.display= "block"
+
+    const tab = document.getElementsByClassName("tab");
+    const showLocationPage = document.getElementById("showLocationPage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showLocationPage.style.color= "#990000";
+}
+
+function showContactPage(){
+    const page = document.getElementsByClassName("page");
+    const contactPage = document.getElementById("contactPage");
+
+    for(var i=0; i<page.length; i++){
+        page[i].style.display = "none"
+    }
+
+    contactPage.style.display= "block"
+
+    const tab = document.getElementsByClassName("tab");
+    const showContactPage = document.getElementById("showContactPage");
+
+    for(var i=0; i<tab.length; i++){
+        tab[i].style.color = "grey";
+    }
+
+    showContactPage.style.color= "#990000";
+}
